1717b3e5da2e8830b873e8b97fe53a62d9771a76
dk19999 <rattandeepak1999@gmail.com>
2022-11-08 22:48:52 +0530

    feat: add mongoose setup

---
+++ environment.d.ts
@@ -0,0 +1,11 @@
+declare namespace globalThis {
+    namespace NodeJS {
+      interface ProcessEnv {
+        NEXT_PUBLIC_MONGODB_URI: string; // this is the line you want
+        NODE_ENV: 'development' | 'production';
+        PORT?: string;
+      }
+    }
+  }
+  
+  
