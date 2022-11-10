e45e2f537b79980539fe19d3bca80ed91f83d6ae
dk19999 <rattandeepak1999@gmail.com>
2022-11-10 20:20:48 +0530

    feat: add paste and user schema

---
+++ models/user.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "../../types";
+
+export type UserType = IUser & Document
+
+const UserSchema:Schema = new Schema<UserType>({
+    name:{
+        type:String,
+        required:[true, 'name must be provided'],
+		minlength:3,
+		maxlength:50
+
+    },
+    email:{
+        type:String,
+        required:[true, 'email must be provided'],
+		match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		'Please provide valid email'],
+		unique:true,
+        lowercase:true
+
+    },
+    password:{
+        type:String,
+        required:[true, 'Please provide password'],
+        minlength:8
+    },
+    role:{
+        type:String,
+        enum:['admin', 'member'],
+        default:'member'
+    }
+
+})
+
+export default mongoose.models.Pastes  ||  mongoose.model('User', UserSchema)
---
+++ types.d.ts
@@ -0,0 +1,23 @@
+import { Types } from "mongoose";
+
+enum Exposure {
+  PUBLIC,
+  PRIVATE,
+  UNLISTED,
+}
+
+interface IPaste {
+  title?: string;
+  body: string;
+  exposure?: Exposure;
+  syntaxLanguage: string;
+  createdBy?: Types.ObjectId;
+  expireAt?: Date;
+}
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  role:string;  
+}
