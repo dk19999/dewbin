5b066a2fa0ce458a202d2fe39cd11d2ffd23b29b
dk19999 <rattandeepak1999@gmail.com>
2022-11-22 01:18:01 +0530

    feat: add custom scalar date type to schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,31 @@
 
 import PasteQuery from './paste/query'
 import PasteMutation from './paste/mutation'
+import { GraphQLScalarType, Kind, ValueNode} from 'graphql'
+
+const DateTime = new GraphQLScalarType({
+  name:'Date',
+  description:'A valid date',
+  parseValue: (value:any) =>  {
+    console.log("🚀 ~ file: index.ts ~ line 17 ~ value", value)
+    return new Date(value) // value from the client
+  },
+  serialize: (value:any) => {
+    console.log("🚀 ~ file: index.ts ~ line 21 ~ serialize", value)
+    const date = new Date(value)
+    return date.toISOString()
+  },
+  parseLiteral: (ast:ValueNode) => {
+    if (ast.kind === Kind.INT) {
+      console.log(new Date(+ast.value))
+      return new Date(+ast.value) // ast value is always in string format
+    }
+    return null
+  }
+
+})
+
+
 
 
 export const resolvers =  {
@@ -10,4 +35,5 @@ export const resolvers =  {
     Mutation:{
         ...PasteMutation
     },
+    DateTime:DateTime
 }
