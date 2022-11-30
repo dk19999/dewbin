948a1908421a8ef171f558ea4593130783ffeca3
dk19999 <rattandeepak1999@gmail.com>
2022-11-30 18:37:41 +0530

    add: setup react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import { useState } from "react";
 import PasteContext, { pasteCtxDefaultValue } from "../contexts/paste";
-import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
+const client = new QueryClient();
+export default function App({ Component, pageProps }:AppProps) {
   const [pasteState, setPasteState] = useState(
     pasteCtxDefaultValue?.pasteState
   );
 
+
   return (
-    <PasteContext.Provider value={{ pasteState, setPasteState }}>
+    <QueryClientProvider client={client}>
+      <PasteContext.Provider value={{ pasteState, setPasteState }}>
       <Component {...pageProps} />
-    </PasteContext.Provider>
+      </PasteContext.Provider>
+    </QueryClientProvider>
   );
 }
