bd66637d118419c6bbe8bfb95fc89c13a0920449
dk19999 <rattandeepak1999@gmail.com>
2022-11-30 18:28:49 +0530

    feat: add paste context

---
+++ src/contexts/paste.ts
@@ -0,0 +1,42 @@
+import React, { Dispatch } from "react";
+
+enum Exposure {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC',
+  Unlisted = 'UNLISTED'
+}
+
+
+export interface IPasteContextState {
+  title: string;
+  body: string;
+  exposure: Exposure;
+  expiration: number;
+  syntaxLanguage: string;
+  link?: string;
+  selectedLanguageData?: any[];
+}
+
+export interface IPasteContext {
+  pasteState: IPasteContextState;
+  setPasteState: Dispatch<React.SetStateAction<any>>;
+}
+
+export const pasteCtxDefaultValue = {
+  pasteState: {
+    title: "",
+    body: "",
+    exposure: Exposure["Public"],
+    syntaxLanguage: "None",
+    expiration: 0,
+    selectedLanguageData: [],
+    link: "",
+  },
+  setPasteState: () => {},
+};
+
+const PasteContext = React.createContext<IPasteContext | null>(
+  null
+);
+
+export default PasteContext;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { useState } from "react";
+import PasteContext, { pasteCtxDefaultValue } from "../contexts/paste";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [pasteState, setPasteState] = useState(
+    pasteCtxDefaultValue?.pasteState
+  );
+
+  return (
+    <PasteContext.Provider value={{ pasteState, setPasteState }}>
+      <Component {...pageProps} />
+    </PasteContext.Provider>
+  );
 }
