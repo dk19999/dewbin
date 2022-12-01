c97517e6263cce3970aa43ed2647483b85020090
dk19999 <rattandeepak1999@gmail.com>
2022-12-01 13:55:51 +0530

    feat: add codemirror editor to home page

---
+++ src/components/editor/editor.tsx
@@ -0,0 +1,29 @@
+import { Text } from "@codemirror/state";
+import React, { useCallback, useContext } from "react";
+import PasteContext, { IPasteContextState } from "../../contexts/paste";
+import useCodeMirror from "../../hooks/use-codemirror";
+import styles from "./editor.module.css";
+const Editor = (props: { editable?: boolean; initialDoc: string | undefined  }) => {
+  const { initialDoc } = props;
+  const pasteContextValue = useContext(PasteContext);
+  const handleChange = useCallback(
+    (state: { doc: Text }) => {
+      const pasteBody = state?.doc?.toString();
+      pasteContextValue?.setPasteState((state:IPasteContextState) => ({ ...state, body: pasteBody }));
+      console.log(
+        "🚀 ~ file: editor.jsx ~ line 14 ~ Editor ~ state?.doc?.toString()",
+        pasteBody
+      );
+    },
+
+    [pasteContextValue?.setPasteState]
+  );
+  const [refContainer] = useCodeMirror<HTMLDivElement>({
+    initialDoc,
+    onChange: handleChange,
+    editable: props.editable ?? true,
+  });
+  return <div className={styles.editor} ref={refContainer}></div>;
+};
+
+export default Editor;
---
+++ src/hooks/use-codemirror.ts
@@ -0,0 +1,75 @@
+import { useContext, useEffect, useRef } from "react";
+import { EditorState, StateEffect } from "@codemirror/state";
+import { EditorView, highlightActiveLine } from "@codemirror/view";
+import { history } from "@codemirror/commands";
+import { indentOnInput } from "@codemirror/language";
+import PasteContext from "../contexts/paste";
+
+export const transparentTheme = EditorView.theme({
+  "&": {
+    backgroundColor: "transparent !important",
+    height: "100%",
+  },
+});
+
+interface Props {
+  initialDoc: string | undefined;
+  onChange: (state: EditorState) => void;
+  editable: boolean;
+}
+
+const useCodeMirror = <T extends Element>(props: Props) => {
+  const refContainer = useRef<T>(null);
+  const pasteContextValue = useContext(PasteContext);
+  const pasteState = pasteContextValue?.pasteState;
+  const { onChange, editable: isEditorEditable } = props;
+
+  let editor = useRef<EditorView | null>(null);
+  useEffect(() => {
+    if (props.initialDoc === undefined || !refContainer.current) {
+      return;
+    }
+
+    const extensions = [
+      history(),
+      indentOnInput(),
+
+      ...(!pasteState?.selectedLanguageData
+        ? []
+        : pasteState.selectedLanguageData),
+      /* Making the background transparent. */
+      transparentTheme,
+      highlightActiveLine(),
+      EditorView.updateListener.of((update) => {
+        if (update.changes) {
+          onChange && onChange(update.state);
+        }
+      }),
+      EditorView.editable.of(isEditorEditable),
+      EditorView.lineWrapping,
+    ];
+    if (!editor.current) {
+      const newEditorView = new EditorView({
+        state: EditorState.create({
+          doc: props.initialDoc,
+          extensions,
+        }),
+        parent: refContainer.current,
+      });
+      editor.current = newEditorView;
+    } else {
+      editor?.current?.dispatch({
+        effects: StateEffect.reconfigure.of(extensions),
+      });
+    }
+  }, [
+    refContainer,
+    editor,
+    pasteState?.selectedLanguageData,
+    props.initialDoc,
+  ]);
+
+  return [refContainer];
+};
+
+export default useCodeMirror;
