f12e35f4351c79607f9b73520a600333d33fc44b
dk19999 <rattandeepak1999@gmail.com>
2022-12-02 14:54:32 +0530

    feat: add download paste feature to view-paste page

--- src/pages/view/[id]/layout.tsx
+++ src/pages/view/[id]/layout.tsx
@@ -1,14 +1,32 @@
 import Layout from "../../../components/layout/index";
 import Header from "../../../components/header/header";
-import { ReactNode } from "react";
+import { ReactNode, useContext, useRef } from "react";
+import PasteContext from "../../../contexts/paste";
+import styles from "./layout.module.css";
 import LanguagesDropdown from "../../../components/language-dropdown/language-dropdown";
 function LayoutWithQuery({ children }: { children: ReactNode }) {
+  const ref = useRef<HTMLAnchorElement>(null);
+  const pasteContextValue = useContext(PasteContext);
+  const { pasteState } = pasteContextValue ?? {};
   return (
     <Layout>
       <Header>
         <LanguagesDropdown />
 
-        {/* {userId === paste.createdBy ?<button>Edit</button>:null} */}
+        <a
+          href=""
+          ref={ref}
+          download="code.txt"
+          className={styles?.["download-btn"]}
+          onClick={function () {
+            if (ref.current)
+              ref.current.href =
+                "data:text/plain;charset=utf-11," +
+                encodeURIComponent(pasteState?.body || "");
+          }}
+        >
+          Download
+        </a>
       </Header>
       {children}
     </Layout>
