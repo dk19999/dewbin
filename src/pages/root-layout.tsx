3797bc3b8425167d9a12419ce6451f1ff3cb9917
dk19999 <rattandeepak1999@gmail.com>
2022-12-02 15:38:00 +0530

    feat: add 'create-paste' feature to home page

--- src/pages/root-layout.tsx
+++ src/pages/root-layout.tsx
@@ -1,13 +1,47 @@
 import Layout from "../components/layout/index";
 import Header from "../components/header/header";
-import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { ReactNode, useContext, useState } from "react";
+import PasteContext from "../contexts/paste";
+import { useCreatePasteMutation } from "../generated/client-graphql";
 import LanguagesDropdown from "../components/language-dropdown/language-dropdown";
 
 function RootLayout({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const contextValue = useContext(PasteContext);
+  const [error, setError] = useState("");
+  const {
+    mutate: createPaste,
+    isSuccess,
+    data,
+    reset,
+  } = useCreatePasteMutation({});
+
+  if (data?.pasteCreate && isSuccess) {
+    router.push(`/view/${data.pasteCreate?.link}`);
+    reset();
+  }
+  const generateLink = () => {
+    if (contextValue?.pasteState) {
+      const { selectedLanguageData, link, ...pasteData } =
+        contextValue.pasteState;
+      if (!pasteData.body) {
+        return setError("Body can't be empty");
+      } else {
+        setError("");
+      }
+      
+      createPaste({ input: pasteData });
+    }
+  };
+
   return (
     <Layout>
       <Header>
-      <LanguagesDropdown />
+        <LanguagesDropdown />
+        <button className="button" onClick={generateLink}>Generate link</button>
+        <span className="error">{error}</span>
+        {/* {userId === paste.createdBy ?<button>Edit</button>:null} */}
       </Header>
       {children}
     </Layout>
